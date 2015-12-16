8cee36591cb5f671ec65248cc449ee65936582df
hxc <173315279@qq.com>
2015-12-16 22:25:38 +0800

    20151216

--- application/install/controller/IndexController.php
+++ application/install/controller/IndexController.php
@@ -10,6 +10,34 @@ class IndexController extends CommonController {
      * 安装首页
      */
     public function indexAction() {
+        //特定样式
+        $this->view->style = 'index';
+        if($this->request->isPost()) {
+            $accept = $this->request->getPost('accept');
+            
+            if(empty($accept)) {
+                return $this->flash->error('请先同意亲');
+            }
+            return $this->response->redirect('install/index/check');
+        }
+        
         
     }
+    
+    /**
+     * 环境检测
+     */
+    public function checkAction() {
+        $this->view->style = 'check';
+                
+    }
+    
+    /**
+     * 同意
+     */
+    public function acceptAction() {
+       
+    }
+    
+    
 }
